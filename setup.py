647978d608cc94d7abfe32c0c8f2cc92d6a677c7
Sebastian Birk <sebastian.birk@outlook.com>
2020-04-25 16:10:40 +0200

    Added code to upload package to PyPi.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='stats_distributions_utils',
       version='0.1',
       description='Gaussian distributions',
-      packages=['distributions'],
+      packages=['stats_distributions_utils'],
+      author='Sebastian Birk',
+      author_email='sebastian.birk@outlook.com',
       zip_safe=False)
---
+++ stats_distributions_utils/README.md
@@ -0,0 +1,11 @@
+# stats_distributions_utils
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
+Explanation for Installation
